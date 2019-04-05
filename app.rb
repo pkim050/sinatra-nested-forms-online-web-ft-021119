b9ee8293665d0121d8fbb67fa7c7aef163f7d2d0
Patrick Kim <pkim050@gmail.com>
2019-04-05 08:23:56 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -4,6 +4,19 @@ module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
+    get '/' do
+      erb :root
+    end
+
+    post '/pirates' do
+      @pirate = Pirate.new(params[:pirate])
+      params[:pirate][:ships].each do |element|
+        Ship.new(element)
+      end
+      @ships = Ship.all
+
+      erb :show
+    end
 
   end
 end
